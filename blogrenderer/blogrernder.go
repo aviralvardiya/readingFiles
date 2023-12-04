ebb48af04d5acf66692ef8fc3cbc69a4119f408a
Aviral Vardiya <aviralvardiya10@gmail.com>
2023-12-04 20:55:25 +0530

    Templating

---
+++ blogrenderer/blogrernder.go
@@ -0,0 +1,59 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+
+
+
+// func Render(w io.Writer, p Post) error {
+// 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if err := templ.ExecuteTemplate(w,"blog.gohtml", p); err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
